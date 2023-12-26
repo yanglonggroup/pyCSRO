4af0298c9ad74973e3cc457c31f310b121d0e658
‘Changyuan <719295379@qq.com>
2023-12-26 21:25:14 +0800

    ENH: setup package files.

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 import setuptools
 
-with open("README.rst", "r") as fh:
+with open("README.md", encoding="utf-8") as fh:
     long_description = fh.read()
 
-
 setuptools.setup(
-    name="pkgname", # Replace with your own name
-    version="0.0.1",
-    author="example author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="pycsro", # Replace with your own name
+    version="0.1.0",
+    description="A python package for CSRO paramater calculation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/yanglonggroup/pyCSRO",
     packages=setuptools.find_packages(),
-    package_dir={"pkgname": "pkgname"},
+    package_dir={"pycsro": "pycsro"},
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
     ],
     entry_points={'console_scripts': [
-            'pkgname = pkgname.main:main',],
+            'pycsro = pycsro.main:run_pycsro_pmsro',],
         },
     data_files = [("", ["LICENSE.txt"])],
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     zip_safe=False,
 )
